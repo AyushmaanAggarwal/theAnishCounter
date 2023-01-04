3a6b55209d5fcec3896479e40bb6a3cd0b55fa84
AyushmaanAggarwal <ayu02468@gmail.com>
2023-01-04 08:56:36 -0800

    Created automationScript with startup and pulling changes

--- automateServer.sh
+++ automateServer.sh
@@ -3,10 +3,19 @@
 # This script will automate the starting and running of the flask server and python virtual enviroment venv
 # Written by: Ayushmaan Aggarwal
 
+# Move to right directory
+cd /home/ayu/theAnishCounter
+
+# Pull existing changes
+git checkout main
+git pull 
+
 # Start virtual enviroment
 source venv/bin/activate
 
 # Install missing packages
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# run flask server
+# run flask server 
+# (python file is run.py and app is the app variable)
+gunicorn -w 9 run:app
