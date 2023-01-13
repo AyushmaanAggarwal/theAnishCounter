869a410fbf65fcfa9468a8c50dad8cb9c1bd6731
Austin Zhu <austin0707@berkeley.edu>
2023-01-13 03:07:24 -0800

    User database and basic interactivity with sign up page implemented, the current page designs are mostly borrowed from a tutorial and should be changed later

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -12,4 +16,24 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        full_name = request.form.get('fullName')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif (password1 != password2):
+            flash('Passwords don\'t match.', category='error')
+        else:
+            new_user = User(fullName=full_name, username = username, email=email, 
+            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html", )
