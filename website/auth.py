9dd4986c207447e1791593019e12e055bab0c9a8
Austin Zhu <austin0707@berkeley.edu>
2023-01-13 02:20:42 -0800

    Added base functionality including a navigation bar that I stole from a tutorial

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    return render_template("sign_up.html", )
