1ace33f64dd492c7c71ce4ea0fef65f1fe2d7e34
AyushmaanAggarwal <ayu02468@gmail.com>
2023-01-15 03:43:09 -0800

    Working on email authorization code(currently broken)

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import random
 
 auth = Blueprint('auth', __name__)
 
@@ -20,6 +21,7 @@ def sign_up():
         full_name = request.form.get('fullName')
         username = request.form.get('username')
         email = request.form.get('email')
+        email_auth = random.uniform(100000, 999999)
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -29,11 +31,31 @@ def sign_up():
         elif (password1 != password2):
             flash('Passwords don\'t match.', category='error')
         else:
-            new_user = User(fullName=full_name, username = username, email=email, 
+            new_user = User(fullName=full_name, username = username, email=email, emailauth=email_auth,
             password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            #TODO: Redirect to email verificication (automate sending email to user with code and prompt user for code before activation)
+            #Make
+            return redirect(url_for('auth.verify_email'))
     return render_template("sign_up.html", )
+
+@auth.route('/verify-email', methods=['GET', 'POST'])
+def verify_email():
+    if request.method == 'POST':
+        email_auth = request.form.get('emailcode')
+
+        user = User.query.filter_by(email=current_user.email).first()
+        #123456 is a temporary bypass while debugging(will disable for final version
+        if (user.emailauth != email_auth or email_auth == 123456):
+            flash('Incorrect Verification Code.', category='error')
+        else:
+            setattr(user, emailauth, 0)
+            db.session.commit()
+            flash('Account verified!', category='success')
+            #TODO: Redirect to email verificication (automate sending email to user with code and prompt user for code before activation)
+            #Make
+            return redirect(url_for('views.home'))
+    return render_template("verify_email.html", )
---
+++ website/emailVerification.py
@@ -0,0 +1,71 @@
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools, file
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+import mimetypes
+from email.mime.base import MIMEBase
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.cache/python/.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'credentials.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    if attachmentFile:
+        message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
+    else:
+        message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+    result = SendMessageInternal(service, "me", message1)
+    return result
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    return {'raw': base64.urlsafe_b64encode(msg.as_bytes())}
+
+def main():
+    to = "ayu02468@gmail.com"
+    sender = "theAnishCounter@gmail.com"
+    subject = "subject"
+    msgHtml = "Hi<br/>Html Email"
+    msgPlain = "Hi\nPlain Email"
+    SendMessage(sender, to, subject, msgHtml, msgPlain)
+
+if __name__ == '__main__':
+    main()
