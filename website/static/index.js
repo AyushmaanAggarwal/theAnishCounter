ccf5ddb996d0958258773e06193a4d61a52bedfd
Austin Zhu <austin0707@berkeley.edu>
2023-01-14 18:44:49 -0800

    Added movie list (any user can only like a movie once), created a remove counter feature

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,29 @@ function increaseCounter(counterId) {
         window.location.href = "/counter";
     });
 }
+
+function deleteCounter(counterId) {
+    fetch("/delete-counter", {
+        method: 'POST',
+        body: JSON.stringify({ counterId: counterId }),
+    }).then((_res) => {
+        window.location.href = "/counter";
+    });
+}
+
+function likeMovie(movieId) {
+    fetch("/like-movie", {
+        method: 'POST',
+        body: JSON.stringify({ movieId: movieId }),
+    }).then((_res) => {
+        window.location.href = "/movies";
+    });
+}
+function unlikeMovie(movieId) {
+    fetch("/unlike-movie", {
+        method: 'POST',
+        body: JSON.stringify({ movieId: movieId }),
+    }).then((_res) => {
+        window.location.href = "/movies";
+    });
+}
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Counter
+from .models import Counter, Movie
 from . import db
 import json
 
@@ -26,7 +26,7 @@ def new_counter():
 
         counter = Counter.query.filter_by(counterName=counterName).first()
         if counter:
-            flash("Counter already existed with that name.", category='error')
+            flash("Counter already exists with that name.", category='error')
         elif len(counterName) == 0:
             flash("Please add a name.", category='error')
         elif len(description) == 0:
@@ -46,5 +46,66 @@ def increase_counter():
     counter = Counter.query.get(counterId)
     if counter:
         counter.counts = counter.counts + 1
+        db.session.commit()
+        return jsonify({})
+
+@views.route('/delete-counter', methods=['POST'])
+def delete_counter():
+    counter = json.loads(request.data)
+    counterId = counter['counterId']
+    counter = Counter.query.get(counterId)
+    if counter:
+        db.session.delete(counter)
+        db.session.commit()
+        return jsonify({})
+
+@views.route('/movies')
+@login_required
+def movies():
+    movies = Movie.query.all()
+    return render_template("movies.html", user=current_user, movies=movies)
+
+@views.route('/add-movie', methods=['POST', 'GET'])
+@login_required
+def add_movie():
+    if request.method == 'POST':
+        movieName = request.form.get('movieName')
+        releaseYear = request.form.get('releaseYear')
+
+        movie = Movie.query.filter_by(movieName=movieName, releaseYear=releaseYear).first()
+        if movie:
+            flash("Movie has already been suggested.", category='error')
+        elif len(movieName) == 0:
+            flash("Please add a name.", category='error')
+        elif releaseYear == 0:
+            flash("Please add a year of release.", category='error')
+        else:
+            new_movie = Movie(movieName=movieName, releaseYear=releaseYear, description="", 
+                              runtime="", likes=0)
+            db.session.add(new_movie)
+            db.session.commit()
+            flash("Movie added!", category='success')
+            return redirect(url_for('views.movies'))
+    return render_template("add_movie.html", user=current_user)
+
+@views.route('/like-movie', methods=['POST'])
+def like_movie():
+    movie = json.loads(request.data)
+    movieId = movie['movieId']
+    movie = Movie.query.get(movieId)
+    if movie:
+        movie.likes = movie.likes + 1
+        movie.likedUsers.append(current_user)
+        db.session.commit()
+        return jsonify({})
+
+@views.route('/unlike-movie', methods=['POST'])
+def unlike_movie():
+    movie = json.loads(request.data)
+    movieId = movie['movieId']
+    movie = Movie.query.get(movieId)
+    if movie:
+        movie.likes = movie.likes - 1
+        movie.likedUsers.remove(current_user)
         db.session.commit()
         return jsonify({})
